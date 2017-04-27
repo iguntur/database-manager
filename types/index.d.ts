d436881ca82ea2f349d8e7a7d0d6cf255db0cdbe
Guntur Poetra <poetra.guntur@gmail.com>
2017-04-27 12:12:47 +0700

    tweaks interfaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -27,23 +27,40 @@ type TableOptions = {
 }
 
 interface QueryBuilderInterface {
-	raw: (query: string) => QueryBuilderInterface;
-	createTable: (table: string) => QueryBuilderInterface;
-	alterTable: () => QueryBuilderInterface;
-	addColumn: (field: string) => QueryBuilderInterface;
-	dropColumn: (field: string) => QueryBuilderInterface;
-	modifyColumn: (field: string) => QueryBuilderInterface;
-	increment: (field: string) => QueryBuilderInterface;
-	string: (field: string, size?: number) => QueryBuilderInterface;
-	integer: (field: string, size?: number) => QueryBuilderInterface;
+	raw: (query: string) => this;
+	createTable: (table: string) => this;
+	alterTable: (table: string) => this;
+	addColumn: (type: string, field: string) => this;
+	dropColumn: (field: string) => this;
+	modifyColumn: (field: string) => this;
+}
+
+interface MigrationColumnInterface {
+	first: () => this;
+	last: () => this;
+	after: (column: string) => this;
+	before: (column: string) => this;
+	default: (value: any) => this;
+	nullable: () => this;
+	unsigned: () => this;
+	comment: (comment: string) => this;
+}
+
+interface MigrationBaseInterface {
+	primary: (field: string) => this;
+	increment: (field: string) => this;
+	string: (field: string, size?: number) => this;
+	integer: (field: string, size?: number) => this;
 	timestamp: (field?: 'created_at' | 'updated_at') => void;
 }
 
 interface SchemaInterface {
-	create: (table: string, callable: (Blueprint: QueryBuilderInterface) => void) => void;
+	create: (table: string, callable: (implement: MigrationBaseInterface) => void) => void;
 	drop: (table: string) => void;
 	dropIfExist: (table: string) => void;
 }
+
+interface SchemaBuilderInterface extends QueryBuilderInterface {}
 
 interface DatabaseManagerInterface {
 	setConnection: (options: ConnectionOptions) => void;
